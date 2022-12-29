5b81b86559ac1067033870a51e1f839af8a40049
sneha gupta <92622110+snehagupta20@users.noreply.github.com>
2022-12-29 18:07:30 +0530

    Add files via upload

---
+++ calculator/calculator.js
@@ -0,0 +1,27 @@
+let num1=8;
+let num2=2;
+let result=document.getElementById("sum");
+function add()
+{
+    let cal=8+2
+    result.textContent="sum: "+cal;
+
+}
+function subtract()
+{
+
+    let cal=8-2
+    result.textContent="sum: "+cal;
+}
+function multiply()
+{
+
+    let cal=8*2
+    result.textContent="sum: "+cal;
+}
+function divide()
+{
+
+    let cal=8/2
+    result.textContent="sum: "+cal;
+}
